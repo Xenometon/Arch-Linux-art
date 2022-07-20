ad1bd3dce33823535e7c4d71526c1e1cc69bc639
Xenometon <106739081+Xenometon@users.noreply.github.com>
2022-07-20 19:25:25 +0530

    Add files via upload

---
+++ Pycode.py
@@ -0,0 +1,24 @@
+#A python program to print ASCII art for Arh linux logo (similar to neofetch command).
+
+print("""\                   -`
+                  .o+`
+                 `ooo/
+                `+oooo:
+               `+oooooo:
+               -+oooooo+:
+             `/:-:++oooo+:
+            `/++++/+++++++:
+           `/++++++++++++++:
+          `/+++ooooooooooooo/`
+         ./ooosssso++osssssso+`
+        .oossssso-````/ossssss+`
+       -osssssso.      :ssssssso.
+      :osssssss/        osssso+++.
+     /ossssssss/        +ssssooo/-
+   `/ossssso+/:-        -:/+osssso+-
+  `+sso+:-`                 `.-/+oso:
+ `++:.                           `-/+/
+ .`                                 `""")
+
+ #"The power of Arch, in the terminal of my Computer."
+ 
